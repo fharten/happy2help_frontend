ef76ee88794b4f7cd73c1c93686c7cf710ba7dc3
fharten <florian.harten@pm.me>
2025-09-04 17:08:06 +0200

    added entity placeholder to switch between user and ngo in development

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import NgoProfileForm from './NgoProfileForm';
 import UserProfileForm from './UserProfileForm';
 
-const ProfilePage = () => {
+const ProfilePage = ({ entity = 'user' }) => {
   return (
     <>
-      <UserProfileForm userId='eacf848a-d5bc-48c7-864d-a43688404938' />
-      {/* <NgoProfileForm ngoId='9f8f3507-ab92-4511-b3e4-48a838b12965' /> */}
+      {entity === 'user' ? (
+        <UserProfileForm userId='eacf848a-d5bc-48c7-864d-a43688404938' />
+      ) : (
+        <NgoProfileForm ngoId='9f8f3507-ab92-4511-b3e4-48a838b12965' />
+      )}
     </>
   );
 };
