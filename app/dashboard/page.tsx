f988d4b6e482ff3b41d7119c9f61667cbed85672
Dirk <so0mewhat2@gmail.com>
2025-09-08 15:58:55 +0200

    feat: user detail page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,13 @@
-import NgoDashboard from './NgoDashboard';
-import UserDashboard from './UserDashboard';
+import NgoDashboard from "./NgoDashboard";
+import UserDashboard from "./UserDashboard";
 
-const ProfilePage = ({ entity = 'user' }) => {
+const ProfilePage = ({ entity = "user" }) => {
   return (
     <>
-      {entity === 'user' ? (
-        <UserDashboard userId='70e3f3e2-0800-41a4-9eab-adb0f58a6c13' />
+      {entity === "user" ? (
+        <UserDashboard userId="19398e16-283e-408e-8c1b-460979cd6856" />
       ) : (
-        <NgoDashboard ngoId='8604ce87-0656-4284-9200-3f1732a33cc2' />
+        <NgoDashboard ngoId="345aa229-7677-4249-ad89-8cba5f632476" />
       )}
     </>
   );
